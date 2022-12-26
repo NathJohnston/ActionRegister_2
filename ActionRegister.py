aa6d22b9a035f803209a46c9a0dea35a3188af00
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-26 12:18:56 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -51,7 +51,7 @@ def insert_row_snowflake(hub, truck_class, target_payload, vims_payload):
 streamlit.stop()
    
 if streamlit.button('Create new Action'):
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)
    my_cnx.close()
    streamlit.text(back_from_function)
