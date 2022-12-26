12a83dcd2c198694408b13ea466ba088b5f919f3
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-26 10:55:09 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -1,1 +1,34 @@
+# Import statements
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+#use this for Control of Flow changes - error message handling
+from urllib.error import URLError
 
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# choose the Fruit Name Column as the Index
+#my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Lets's add a pick list here so they can pick the fruit they want to include
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Pre-populate the list with some fruit
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# display the table on the page
+# streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(fruits_to_show)
