e55be39c78df5a70f16c36127952a67aa81f052e
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-26 11:10:26 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -6,29 +6,23 @@ import snowflake.connector
 #use this for Control of Flow changes - error message handling
 from urllib.error import URLError
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Actions and Issues Tracker')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacado Toast')
+#streamlit.header('Breakfast Menu')
+#streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#test snowflake connection
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * FROM tblTruckPayloadTargets")
 
+#my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# choose the Fruit Name Column as the Index
-my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.header("Action/ Issue Register")
+#streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
-# Lets's add a pick list here so they can pick the fruit they want to include
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-# Pre-populate the list with some fruit
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# display the table on the page
-# streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(fruits_to_show)
+# new action variables
+action_date = streamlit.text_input('Action date:')
