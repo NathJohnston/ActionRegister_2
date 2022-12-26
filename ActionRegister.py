eebe4369225d60b5b201d55f3aaf5b086ee9b7b7
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-26 12:17:25 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -17,9 +17,11 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * FROM tblTruckPayloadTargets")
+my_cnx.close()
 
 #my_data_row = my_cur.fetchone()
 my_data_rows = my_cur.fetchall()
+
 
 streamlit.header("Action/ Issue Register")
 #streamlit.dataframe(my_data_row)
@@ -38,12 +40,18 @@ truck_class = streamlit.text_input('Enter truck class:')
 target_payload = streamlit.text_input('Enter target payload:')
 vims_payload = streamlit.text_input('Enter vims payload:')
 
-streamlit.button('Create new Action')
-
-
 # Use a Function and Button to Add new record
 # Allow the end user to add a new record to the action list
 def insert_row_snowflake(hub, truck_class, target_payload, vims_payload):
    with my_cnx.cursor() as my_cur:
       my_cur.execute("INSERT INTO tblTruckPayloadTargets VALUES ('"+ hub +"', '"+ truck_class +"', '"+ target_payload +"', '"+ vims_payload +"')")
       return "Thanks for adding " + truck_class
+
+# don't run anything past here while I troubleshoot
+streamlit.stop()
+   
+if streamlit.button('Create new Action')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   my_cnx.close()
+   streamlit.text(back_from_function)
