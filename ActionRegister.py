faccd3bdadf29dc466391cf79e238d12c176de34
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-26 12:31:57 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -44,7 +44,7 @@ vims_payload = streamlit.text_input('Enter vims payload:')
 # Allow the end user to add a new record to the action list
 def insert_row_snowflake(hub, truck_class, target_payload, vims_payload):
    with my_cnx.cursor() as my_cur:
-      my_cur.execute("INSERT INTO tblTruckPayloadTargets VALUES (11, '"+ hub +"', '"+ truck_class +"', '"+ target_payload +"', '"+ vims_payload +"')")
+      my_cur.execute("INSERT INTO tblTruckPayloadTargets (HUB, TRUCKCLASS, PAYLOADTARGET, VIMS_PAYLOAD) VALUES ('"+ hub +"', '"+ truck_class +"', '"+ target_payload +"', '"+ vims_payload +"')")
       return "Thanks for adding " + truck_class
 
 # don't run anything past here while I troubleshoot
