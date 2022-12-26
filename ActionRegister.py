6575dbe26ddba5c8a089111949c57ad29538573b
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-26 11:03:07 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -26,8 +26,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Pre-populate the list with some fruit
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
-#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the page
 # streamlit.dataframe(my_fruit_list)
