6ef4360374cf583d84b2f66741c0868e604a4f61
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-26 11:04:41 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -31,4 +31,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the page
 # streamlit.dataframe(my_fruit_list)
-#streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
