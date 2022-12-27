88444372d30bd0682a4b493cd081e1e664ef6d48
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-28 05:11:42 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -94,9 +94,9 @@ my_cnx.close()
 
 
 updateAction = action_to_update_cur.fetchmany()
-#for row in updateAction:
-   #ud_action = streamlit.text_input('Action details:',f'{row[2]}')
-   #ud_owner = streamlit.text_input('Owner:',f'{row[3]}')
+for row in updateAction:
+   ud_action = streamlit.text_input('Action details:',f'{row[2]}')
+   ud_owner = streamlit.text_input('Owner:',f'{row[3]}')
    #ud_due_date = streamlit.text_input('Due Date:',f'{row[4]}')
    #ud_status = streamlit.text_input('Status:',f'{row[5]}') 
    #txtstatus = str(f"{row[5]}")
