6b661189692a06a2d1ed2a6926a53306635f0b7f
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-28 05:57:44 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -109,15 +109,16 @@ for row in updateAction:
 # Row E -----------------------------------------------------------------------------------------------------------------   
 e1, e2, e3 = streamlit.columns((4,3,3))
 with e1:
-for row in updateAction:   
-   ud_owner = streamlit.text_input('Owner:',f'{row[3]}')
+   for row in updateAction:   
+      ud_owner = streamlit.text_input('Owner:',f'{row[3]}')
    
 with e2:
-   ud_due_date = streamlit.text_input('Due Date:',f'{row[4]}')
-   ud_status = streamlit.text_input('Status:',f'{row[5]}') 
-   txtstatus = str(f"{row[5]}")
-   #streamlit.write(f"{row[5]}")
-   #ud_status2 = streamlit.selectbox('Current Status:', txtstatus) #('New', 'In Progress', 'Delayed','Complete')
+   for row in updateAction:
+      ud_due_date = streamlit.text_input('Due Date:',f'{row[4]}')
+      ud_status = streamlit.text_input('Status:',f'{row[5]}') 
+      txtstatus = str(f"{row[5]}")
+      #streamlit.write(f"{row[5]}")
+      #ud_status2 = streamlit.selectbox('Current Status:', txtstatus) #('New', 'In Progress', 'Delayed','Complete')
 
 #col1,col2 = streamlit.columns(2)
 #with col1:
