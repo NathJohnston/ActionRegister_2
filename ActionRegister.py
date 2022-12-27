c1f3a0b2d13e0d2f50546ac1cba0bc623222bdbb
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-27 08:39:34 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -83,7 +83,7 @@ my_cnx.close()
 
 streamlit.subheader('Update existing Action')
 try:
-   action_ids = my_id_cur.fetchall(): 
+   action_ids = my_id_cur.fetchall() 
    final_result = [i[0] for i in action_ids]
    select_id = streamlit.selectbox('Select Action ID:',final_result)
                                                                          
