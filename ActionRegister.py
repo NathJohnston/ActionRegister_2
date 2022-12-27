8b2af617136806116aceb0a02c1bb1b338635c67
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-27 10:26:19 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -92,7 +92,7 @@ if streamlit.button('Update Action'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_cur = my_cnx.cursor()
    #update_response = update_selected_action(action, owner, due_date, status)
-   my_cur.execute("UPDATE tbl_OperationalActionsRegister SET Action = '"+ action +"', Owner = '"+ owner +"', DueDate = '"+ due_date +"', Status = '"+ status +"' WHERE Action_ID = " + select_id +"")
+   my_cur.execute("UPDATE tbl_OperationalActionsRegister SET Action = '"+ action +"', Owner = '"+ owner +"', DueDate = '"+ due_date +"', Status = '"+ status +"' WHERE Action_ID = '"+ select_id +"'")
    my_cnx.close()
    streamlit.text('update succeded')
  
