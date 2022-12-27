a5a825e817abfeac70cd24bbde745e653a60c809
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-27 08:58:09 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -86,4 +86,9 @@ streamlit.subheader('Update existing Action')
 
 action_ids = my_id_cur.fetchall() 
 final_result = [i[0] for i in action_ids]
-select_id = streamlit.selectbox('Select Action ID:',final_result)
+try:
+   select_id = streamlit.selectbox('Select Action ID:',final_result)
+   if not select_id:
+      streamlit.error("No Action ID selected.")
+   else:
+      streamlit.text('the following ID was selected:',select_id)
