18a9a973f00b8b0beeac6bce04f1075e39ffe349
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-27 14:57:49 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -94,7 +94,8 @@ my_cnx.close()
 
 
 updateAction = action_to_update_cur.fetchmany()
-#for row in updateAction:
+for row in updateAction:
+   Up_action = streamlit.text_input('Action details:',f"{row[2]}")
 #       Up_action = streamlit.text_input('Action details:',f"{row[2]}")
 
 #col1,col2 = streamlit.columns(2)
