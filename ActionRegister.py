48a1e48f1bf98b85daef9a9aa7f41fe0f714e0b9
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-12-29 13:03:35 +0800

    Update ActionRegister.py

--- ActionRegister.py
+++ ActionRegister.py
@@ -26,6 +26,42 @@ def update_selected_action(ud_action, ud_owner, ud_due_date, ud_status):
       my_cnx.close()
    return str(select_id)
 
+def page_setup():
+   #Set page title
+   streamlit.title('Actions and Issues Tracker')
+   streamlit.write('my password:', streamlit.secrets["password"]) 
+      #Connect to Snowflake and instantiate cursor object
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_dataframe_cur = my_cnx.cursor()
+
+      #Populate the cursor with the data in the tbl_OperationalActionsRegister table using execute and close cursor
+   my_dataframe_cur.execute("SELECT * FROM tbl_OperationalActionsRegister")
+   my_cnx.close()
+
+      #Populate my_data_rows variable with cursor results
+   my_data_rows = my_dataframe_cur.fetchall()
+
+      #Create action table header
+   streamlit.header(':blue[Action/ Issue Register] :runner:')
+
+      #Populate dataframe
+   df = pandas.DataFrame(
+      my_data_rows,
+      columns=("Action ID", "Entry Date", "Action", "Owner", "Due Date", "Status"))
+
+   # CSS to inject contained in a string
+   hide_dataframe_row_index = """
+               <style>
+               .row_heading.level0 {display:none}
+               .blank {display:none}
+               </style>
+               """
+   # Inject CSS with Markdown
+   streamlit.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+
+   streamlit.dataframe(df,width=1500,height=245)
+
+
    #create new action variables and objects in sidebar object
 def buildsidebar():
    
@@ -46,6 +82,58 @@ def buildsidebar():
       if streamlit.button('Create new Action'):
          back_from_function = insert_row_snowflake(action_date, action, owner, due_date, status)
          streamlit.success(back_from_function)
+         
+def update_action():
+      #Create update action header
+   streamlit.subheader(':orange[Update existing Active Action - Select Action ID]')      
+
+      #Retrieve list of active action ID's
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_id_cur = my_cnx.cursor()
+   my_id_cur.execute("SELECT Action_ID FROM tbl_OperationalActionsRegister WHERE Status <> 'Complete'")
+   my_cnx.close()
+
+   b1, b2 = streamlit.columns((1,9))
+      #format the results in the cursor and populate the select box object
+   with b1:
+      action_ids = my_id_cur.fetchall() 
+      final_result = [i[0] for i in action_ids]
+      select_id = streamlit.selectbox('Select Action ID:',final_result, label_visibility="collapsed")
+      #select_id = streamlit.selectbox(:blue[Select Action ID:],('Email', 'Home phone', 'Mobile phone')) -- does not work
+
+   # Row C -----------------------------------------------------------------------------------------------------------------
+      #Retrieve Action based on selected action ID
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   action_to_update_cur = my_cnx.cursor()
+   action_to_update_cur.execute("SELECT * FROM tbl_OperationalActionsRegister WHERE Action_ID = "+ str(select_id) +"")
+   my_cnx.close()
+   updateAction = action_to_update_cur.fetchmany()
+
+   # Row D -----------------------------------------------------------------------------------------------------------------
+   for row in updateAction:
+      ud_action = streamlit.text_input('Action details:',f'{row[2]}')
+
+   # Row E -----------------------------------------------------------------------------------------------------------------   
+   e1, e2, e3 = streamlit.columns((4,3,3))
+   with e1:
+      for row in updateAction:   
+         ud_owner = streamlit.text_input('Owner:',f'{row[3]}')   
+   with e2:
+      for row in updateAction:
+         ud_due_date = streamlit.text_input('Due Date:',f'{row[4]}')
+   with e3:
+      for row in updateAction:
+         ud_status = streamlit.text_input('Status:',f'{row[5]}') 
+
+         #txtstatus = str(f"{row[5]}")
+         #mystring = ' '.join(map(str,f"{row[5]}"))
+         #ud_status2 = streamlit.selectbox('Current Status:', mystring) #('New', 'In Progress', 'Delayed','Complete')
+
+   #Update record for action ID selected in the selected_id selectbox
+   if streamlit.button('Update Action'):
+      back_from_function = update_selected_action(ud_action, ud_owner, ud_due_date, ud_status)
+      streamlit.success('Action ID: ' + str(back_from_function) + ' Update Succeded')
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # -----------------------------------------------------------------------------------------------------------------------
 
 
@@ -86,120 +174,23 @@ def check_password():
         return True
 
 if check_password():
-   streamlit.write("Here goes your normal Streamlit app...")
-    
-    #buildsidebar()
+   #streamlit.write("Here goes your normal Streamlit app...")
    
+   page_setup()
+   buildsidebar()
+   update_action()
    
    
 #======================================================================================================================================================
 
 # Row A -----------------------------------------------------------------------------------------------------------------
-   #Set page title
-streamlit.title('Actions and Issues Tracker')
-streamlit.write('my password:', streamlit.secrets["password"]) 
-   #Connect to Snowflake and instantiate cursor object
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_dataframe_cur = my_cnx.cursor()
 
-   #Populate the cursor with the data in the tbl_OperationalActionsRegister table using execute and close cursor
-my_dataframe_cur.execute("SELECT * FROM tbl_OperationalActionsRegister")
-my_cnx.close()
-
-   #Populate my_data_rows variable with cursor results
-my_data_rows = my_dataframe_cur.fetchall()
-
-   #Create action table header
-streamlit.header(':blue[Action/ Issue Register] :runner:')
-
-   #Populate dataframe
-df = pandas.DataFrame(
-   my_data_rows,
-   columns=("Action ID", "Entry Date", "Action", "Owner", "Due Date", "Status"))
-
-# CSS to inject contained in a string
-hide_dataframe_row_index = """
-            <style>
-            .row_heading.level0 {display:none}
-            .blank {display:none}
-            </style>
-            """
-# Inject CSS with Markdown
-streamlit.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-
-streamlit.dataframe(df,width=1500,height=245)
 
 # Sidebar ----------------------------------------------------------------------------------------------------------------
-   #create new action variables and objects in sidebar object   
-with streamlit.sidebar:
-   streamlit.header(':lower_left_ballpoint_pen: :blue[Enter New Action]')
-   date_select1 = streamlit.date_input('Action date:')
-      #convert the date to the required string format
-   action_date = date_select1.strftime("%m/%d/%Y")
-   action = streamlit.text_input('Action details:','Enter details')
-   owner = streamlit.text_input('Action Owner:','Enter owner details')
-   date_select2 = streamlit.date_input('Action Due Date:')
-      #convert the date to the required string format
-   due_date = date_select2.strftime("%m/%d/%Y")
-   status = streamlit.selectbox('Current Status:', ('New', 'In Progress', 'Delayed','Complete'))
 
-   #Insert new action record based on sliderbar objects
-with streamlit.sidebar:   
-   if streamlit.button('Create new Action'):
-      back_from_function = insert_row_snowflake(action_date, action, owner, due_date, status)
-      streamlit.success(back_from_function)
   
 # Row B -----------------------------------------------------------------------------------------------------------------
-      #Create update action header
-streamlit.subheader(':orange[Update existing Active Action - Select Action ID]')      
 
-   #Retrieve list of active action ID's
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_id_cur = my_cnx.cursor()
-my_id_cur.execute("SELECT Action_ID FROM tbl_OperationalActionsRegister WHERE Status <> 'Complete'")
-my_cnx.close()
-
-b1, b2 = streamlit.columns((1,9))
-   #format the results in the cursor and populate the select box object
-with b1:
-   action_ids = my_id_cur.fetchall() 
-   final_result = [i[0] for i in action_ids]
-   select_id = streamlit.selectbox('Select Action ID:',final_result, label_visibility="collapsed")
-   #select_id = streamlit.selectbox(:blue[Select Action ID:],('Email', 'Home phone', 'Mobile phone')) -- does not work
-
-# Row C -----------------------------------------------------------------------------------------------------------------
-   #Retrieve Action based on selected action ID
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-action_to_update_cur = my_cnx.cursor()
-action_to_update_cur.execute("SELECT * FROM tbl_OperationalActionsRegister WHERE Action_ID = "+ str(select_id) +"")
-my_cnx.close()
-updateAction = action_to_update_cur.fetchmany()
-
-# Row D -----------------------------------------------------------------------------------------------------------------
-for row in updateAction:
-   ud_action = streamlit.text_input('Action details:',f'{row[2]}')
-   
-# Row E -----------------------------------------------------------------------------------------------------------------   
-e1, e2, e3 = streamlit.columns((4,3,3))
-with e1:
-   for row in updateAction:   
-      ud_owner = streamlit.text_input('Owner:',f'{row[3]}')   
-with e2:
-   for row in updateAction:
-      ud_due_date = streamlit.text_input('Due Date:',f'{row[4]}')
-with e3:
-   for row in updateAction:
-      ud_status = streamlit.text_input('Status:',f'{row[5]}') 
-      
-      #txtstatus = str(f"{row[5]}")
-      #mystring = ' '.join(map(str,f"{row[5]}"))
-      #ud_status2 = streamlit.selectbox('Current Status:', mystring) #('New', 'In Progress', 'Delayed','Complete')
-      
-#Update record for action ID selected in the selected_id selectbox
-if streamlit.button('Update Action'):
-   back_from_function = update_selected_action(ud_action, ud_owner, ud_due_date, ud_status)
-   streamlit.success('Action ID: ' + str(back_from_function) + ' Update Succeded')
-   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    
  
 
